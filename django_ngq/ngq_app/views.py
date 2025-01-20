64d9f6b6d590efa0cadb7e9b029df03d8f0b62a8
CasualCodes <95406523+CasualCodes@users.noreply.github.com>
2025-01-20 20:06:06 +0800

    added todo plans for django backend implementation. added an additional example to prompt context

--- django_ngq/ngq_app/views.py
+++ django_ngq/ngq_app/views.py
@@ -3,6 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index():
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Setup Form Pseudocode
+    #   - input 'Enter URL'
+    #   - submit
+    
+    # Index page pseudocode (Core function)
+    # After submit, load loading widget
+    # After loading widget, run scraper->promptgenerator->llm->table_generator pipeline
+    # After running, redirect to results page with data
 
-# def table_view # For Output
+    # Load HTML
+    return HttpResponse("Hello, world. You're at the index page.")
+
+def results(data):
+    # Results Page Pseudocode
+    # Receive data, if fail, show blank
+    # After receiving data, load dataframe to table widget
+
+    # Other Buttons
+
+    # Load HTML
+    return HttpResponse(f"Hello, world. You're at the results page")
