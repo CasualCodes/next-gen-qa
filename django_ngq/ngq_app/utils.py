88acee6270a77cc68a2a92379192ec3881ba9c79
CasualCodes <95406523+CasualCodes@users.noreply.github.com>
2025-01-21 09:31:19 +0800

    added more details to views.py pseudocode for backend implementation purposes

--- django_ngq/ngq_app/utils.py
+++ django_ngq/ngq_app/utils.py
@@ -1,2 +1,12 @@
 # Plug in your logic code here (Scraper, Prompt Generator, Fine Tuned LLM, And Table Generator)
 # TODO : Code Integration and utils.py connection
+
+## Scraper
+
+
+## Prompt Generator + LLM
+
+
+## Table Generator
+
+
--- django_ngq/ngq_app/views.py
+++ django_ngq/ngq_app/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import URLForm
+
 
 # Create your views here.
-def index():
+def index(request):
     # Setup Form Pseudocode
     #   - input 'Enter URL'
     #   - submit
     
     # Index page pseudocode (Core function)
-    # After submit, load loading widget
-    # After loading widget, run scraper->promptgenerator->llm->table_generator pipeline
+    # # if form.is_valid():
+    # - After submit, load loading widget
+    # - After loading widget, run scraper->promptgenerator->llm->table_generator pipeline
+    # # data = generate_table(generate_test_cases(scrape_url(url)))
+    # Save data to session
+    # # request.session['data'] = data
     # After running, redirect to results page with data
+    # # HttpResponseRedirect('results')
 
+    # else : 
     # Load HTML
     return HttpResponse("Hello, world. You're at the index page.")
 
-def results(data):
+def results(request):
     # Results Page Pseudocode
     # Receive data, if fail, show blank
+    # # data = request.session['data']
     # After receiving data, load dataframe to table widget
 
     # Other Buttons
